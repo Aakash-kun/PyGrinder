d13e35d45a0ef86c0dd6716fe8b5d1a4f55a7ac9
koala9712 <party.mittal@gmail.com>
2022-04-21 12:11:42 +0530

    Fix WS Code

--- ws/heartbeat.py
+++ ws/heartbeat.py
@@ -1,20 +1,15 @@
-
-import json
-import aiohttp
 async def heartbeat(instance):
     await instance.ws.send(
-        json.dumps(
-            {
-                "op":1,
-                "d": {
-                    "token": instance.token,
-                    "intents": 4609,
-                    "properties": {
-                        "$os":"windows",
-                        "$browser":"Discord",
-                        "$device": "desktop"
-                    }
+        {
+            "op":1,
+            "d": {
+                "token": instance.token,
+                "intents": 4609,
+                "properties": {
+                    "$os":"windows",
+                    "$browser":"Discord",
+                    "$device": "desktop"
                 }
             }
-        )
+        }
     )
